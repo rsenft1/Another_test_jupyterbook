110300892df96b10ca6bc77970dbceb7db5872a7
Rebecca Senft <28116530+rsenft1@users.noreply.github.com>
2022-03-23 17:47:31 -0400

    add files from test branch

---
+++ runallbook/_build/jupyter_execute/docs/test.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# (launch:thebe)=
+# # test
+
+# In[1]:
+
+
+var1="hello world!"
+
+
+# ```{python tags=c("hide-input")}
+# print(var1)
+# ```
+
+# In[2]:
+
+
+print("This is a test.")
+
+
+# In[3]:
+
+
+print(var1)
+
+
+# In[4]:
+
+
+print(f"This is a test. {var1}")
+
+
+# In[5]:
+
+
+print("Margin code")
+
+
+# In[6]:
+
+
+for ii in range(100):
+  print("This is a test.")
+
